1ef071cf4b55800be9650e11b5d54249e8acfe85
Ian K Smith <smithki707@gmail.com>
2020-12-22 11:20:03 -0500

    Add dynamic template downloading via GitHub

--- src/cli.ts
+++ src/cli.ts
@@ -3,36 +3,22 @@
 import meow from 'meow';
 import execa from 'execa';
 import CFonts from 'cfonts';
-import generator from './generator';
-
-const help = `
-  Usage
-    $ create-magic-app <input>
-
-  Options
-    --version, -v    Show which version of \`create-magic-app\` is currently
-                     in use.
-
-    --help, -h       Show help (you're lookin' at it).
-
-  Examples
-    $ foo unicorns --rainbow
-    🌈 unicorns 🌈
-`;
+import { createApp } from './create-app';
+import { makeDir } from './utils/make-dir';
+import { TEMPLATE_ROOT } from './config';
+import { helpText } from './help-text';
 
 const cli = meow({
-  help,
-
   flags: {
     help: { type: 'boolean', default: false, alias: 'h' },
     version: { type: 'boolean', default: false, alias: 'v' },
   },
+
+  help: helpText,
+  autoHelp: false,
 });
 
-(async () => {
-  if (cli.flags.help) cli.showHelp();
-  if (cli.flags.version) cli.showVersion();
-
+function sayHello() {
   CFonts.say('Create|Magic|App', {
     font: 'tiny',
     align: 'left',
@@ -41,11 +27,28 @@ const cli = meow({
     gradient: ['#6851ff', '#a796ff'],
     transitionGradient: true,
   });
+}
 
-  const props = await generator.run();
-  console.log(); // Add empty line for spacing
+(async () => {
+  if (cli.flags.version) cli.showVersion();
 
-  // Start running the example app!
+  if (cli.flags.help) {
+    sayHello();
+    cli.showHelp();
+  }
+
+  sayHello();
+
+  // Make sure there's at least a root template directory to work from.
+  await makeDir(TEMPLATE_ROOT);
+
+  // Run the scaffold...
+  const props = await createApp();
+
+  // Add an empty line for "aesthetic"
+  console.log();
+
+  // Run the example app we just scaffolded!
   if (props.startCommand) {
     await execa.command(props.startCommand, { stdio: 'inherit' });
   }
