f30a1fcdc715d2e2c2d03a4ddca6b5bbc0d03a5d
Ian K Smith <smithki707@gmail.com>
2020-12-27 11:42:02 -0500

    Update scaffold implementation to match new 'zombi' API

---
+++ src/create-app.tsx
@@ -0,0 +1,171 @@
+/* eslint-disable no-param-reassign */
+
+import React from 'react';
+import { Zombi, Directory, Template, scaffold } from 'zombi';
+import path from 'path';
+import fs from 'fs';
+import { URL } from 'url';
+import execa from 'execa';
+import { downloadAndExtractRepo, getRepoInfo } from './utils/repo';
+import { makeDir } from './utils/make-dir';
+import { DEFAULT_CREATE_MAGIC_APP_REPO, GITHUB_BASE_URL, TEMPLATE_ROOT } from './config';
+
+interface TemplateData {
+  projectName: string;
+  framework: string;
+  database: string;
+  socialLogins: string[];
+  magicPublishableKey: string;
+  magicSecretKey: string;
+  faunaSecretKey?: string;
+}
+
+function getExamplePath(data: Partial<TemplateData>) {
+  return [
+    data.framework,
+    data.database !== 'none' && data.database,
+    data.socialLogins?.length ? 'withsocial' : 'emailonly',
+  ]
+    .filter(Boolean)
+    .join('-');
+}
+
+export async function createApp() {
+  const templateRoot = path.resolve(__dirname, '../templates');
+  const destinationRoot = process.cwd();
+
+  const template = (
+    <Zombi<TemplateData>
+      name="create-magic-app"
+      templateRoot={templateRoot}
+      destinationRoot={destinationRoot}
+      prompts={[
+        {
+          type: 'input',
+          name: 'projectName',
+          message: 'What is your project named?',
+          initial: 'my-app',
+        },
+
+        {
+          type: 'select',
+          name: 'framework',
+          message: 'Choose a full-stack framework:',
+          choices: [
+            { name: 'react-express', message: 'React + Express' },
+            { name: 'nextjs', message: 'NextJS' },
+          ],
+        },
+
+        {
+          type: 'select',
+          name: 'database',
+          message: 'Choose a database:',
+          choices: [
+            { name: 'none', message: 'None' },
+            { name: 'fauna', message: 'FaunaDB' },
+          ],
+        },
+
+        {
+          type: 'multiselect',
+          name: 'socialLogins',
+          message: 'Choose social login providers:',
+          choices: [
+            { name: 'google', message: 'Sign in with Google' },
+            { name: 'facebook', message: 'Sign in with Facebook' },
+            { name: 'github', message: 'Sign in with GitHub' },
+          ],
+        },
+
+        {
+          type: 'input',
+          name: 'magicPublishableKey',
+          message: `Paste your Magic public API key:`,
+        },
+
+        {
+          type: 'password',
+          name: 'magicSecretKey',
+          message: `Paste your Magic secret API key:`,
+        },
+
+        (data) => [
+          data.database.includes('fauna') && {
+            type: 'password',
+            name: 'faunaSecretKey',
+            message: `Paste your FaunaDB secret:`,
+          },
+        ],
+      ]}
+      onPromptResponse={async (data) => {
+        const example = getExamplePath(data);
+        const repoUrl = new URL(DEFAULT_CREATE_MAGIC_APP_REPO, GITHUB_BASE_URL);
+        const repoInfo = await getRepoInfo(repoUrl, path.join('templates', example));
+
+        if (repoInfo) {
+          const root = path.resolve(templateRoot, example);
+
+          if (!fs.existsSync(root)) {
+            await makeDir(root);
+            await downloadAndExtractRepo(root, repoInfo);
+          }
+        } else {
+          // TODO: Handle case where repo info is not found
+        }
+      }}
+    >
+      {(data) => (
+        <Directory name={data.projectName}>
+          <Template source={getExamplePath(data)} name="." />
+        </Directory>
+      )}
+    </Zombi>
+  );
+
+  const { data: globalData } = await scaffold<TemplateData>(template);
+  const { 'create-magic-app': data } = globalData;
+
+  // Move the current working directory to the rendered scaffold.
+  process.chdir(data.projectName);
+
+  // Do post-render actions.
+  await installDependencies(data);
+  await runApp(data);
+}
+
+/**
+ * After the scaffold is rendered, we call this function to install any
+ * dependencies the example app requires.
+ */
+async function installDependencies(data: TemplateData) {
+  const installCommands: Record<string, string> = {
+    'react-express': 'yarn install',
+    nextjs: 'yarn install',
+  };
+
+  const framework = Object.keys(installCommands).find((f) => data.framework === f)!;
+  const installCommand = installCommands[framework];
+
+  if (installCommand) {
+    await execa.command(installCommand, { stdio: 'inherit' });
+  }
+}
+
+/**
+ * After the scaffold is rendered, we call this function to start the example
+ * app, providing instant gratification for first-time Magic developers!
+ */
+async function runApp(data: TemplateData) {
+  const startCommands: Record<string, string> = {
+    'react-express': 'yarn start',
+    nextjs: 'yarn dev',
+  };
+
+  const framework = Object.keys(startCommands).find((f) => data.framework === f)!;
+  const startCommand = startCommands[framework];
+
+  if (startCommand) {
+    await execa.command(startCommand, { stdio: 'inherit' });
+  }
+}
