4c7ee2ad4a722697676e2e7703ce1cf036af2075
David Gerrells <david.gerrells@magic.link>
2021-04-23 10:28:44 -0700

    move stripe prompts to chared prompts file

--- scaffolds/magic-stripe/scaffold.tsx
+++ scaffolds/magic-stripe/scaffold.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { Template, Zombi } from 'compiled/zombi';
 import { createScaffold } from 'cli/utils/scaffold-helpers';
 import { mergePrompts } from 'cli/utils/merge-prompts';
-import { NpmClientPrompt, PublishableApiKeyPrompt, SecretApiKeyPrompt } from 'scaffolds/prompts';
-import { StripePublishableApiKeyPrompt, StripeSecretApiKeyPrompt } from './prompts';
+import {
+  NpmClientPrompt,
+  PublishableApiKeyPrompt,
+  SecretApiKeyPrompt,
+  StripeSecretApiKeyPrompt,
+  StripePublishableApiKeyPrompt,
+} from 'scaffolds/prompts';
 
 type StripeMagicData = NpmClientPrompt.Data &
   PublishableApiKeyPrompt.Data &
