c8c3f2eea3aba6a9c06f036030756bdb82b4b61a
Ethella <jerry@fortmatic.com>
2021-04-27 07:49:47 -0700

    Add new referral template

---
+++ scaffolds/hello-world-referral/scaffold.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Template, Zombi } from 'compiled/zombi';
+import { createScaffold } from 'cli/utils/scaffold-helpers';
+import { mergePrompts } from 'cli/utils/merge-prompts';
+import { NpmClientPrompt, PublishableApiKeyPrompt } from 'scaffolds/prompts';
+
+type HelloWorldData = NpmClientPrompt.Data & PublishableApiKeyPrompt.Data;
+
+export default createScaffold<HelloWorldData>(
+  (props) => (
+    <Zombi {...props} prompts={mergePrompts(PublishableApiKeyPrompt.questions, NpmClientPrompt.questions)}>
+      <Template source="./" />
+    </Zombi>
+  ),
+
+  {
+    shortDescription: 'Hello World',
+    order: 0,
+    installDependenciesCommand: NpmClientPrompt.getInstallCommand,
+    startCommand: NpmClientPrompt.getStartCommand('start'),
+    flags: {
+      ...NpmClientPrompt.flags,
+      ...PublishableApiKeyPrompt.flags,
+    },
+  },
+);
