a6292d206899164649fc4fe3f1a6da04b8a06c97
James Pacheco <jamesrp13@gmail.com>
2023-08-10 15:26:54 -0600

    Update connect template w/ api key instructions

--- scaffolds/nextjs-magic-connect/template/src/pages/index.tsx
+++ scaffolds/nextjs-magic-connect/template/src/pages/index.tsx
@@ -1,18 +1,28 @@
-import React, {useState, useEffect} from 'react'
-import Login from '../components/magic/Login'
-import Home from '../components/magic/Home'
+import React, { useState, useEffect } from 'react';
+import Login from '../components/magic/Login';
+import Home from '../components/magic/Home';
+import MagicDashboardRedirect from '@/components/magic/MagicDashboardRedirect';
 
 export default function App() {
-	const [account, setAccount] = useState<string | null>(null)
+  const [account, setAccount] = useState<string | null>(null);
 
-	useEffect(() => {
-		const user = localStorage.getItem('user')
-		setAccount(user)
-	}, [])
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    setAccount(user);
+  }, []);
 
-	return !account ? (
-		<Login setAccount={setAccount} />
-	) : (
-		<Home setAccount={setAccount} />
-	)
+  /**
+   * If no Magic API key is set, instruct user to go to Magic Dashboard.
+   * Otherwise, if user is not logged in, show the Login component.
+   * Otherwise, show the Home component.
+   */
+  return process.env.NEXT_PUBLIC_MAGIC_API_KEY ? (
+    !account ? (
+      <Login setAccount={setAccount} />
+    ) : (
+      <Home setAccount={setAccount} />
+    )
+  ) : (
+    <MagicDashboardRedirect />
+  );
 }
