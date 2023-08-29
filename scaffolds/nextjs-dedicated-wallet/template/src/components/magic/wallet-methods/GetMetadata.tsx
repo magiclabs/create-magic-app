45031727c62d0ec8fb811459d32cfbfbf8e815a3
Aditya Kulkarni <kulkarniaditya1997@gmail.com>
2023-08-29 14:30:37 -0700

    template sync fixes and css refactor

--- scaffolds/nextjs-dedicated-wallet/template/src/components/magic/wallet-methods/GetMetadata.tsx
+++ scaffolds/nextjs-dedicated-wallet/template/src/components/magic/wallet-methods/GetMetadata.tsx
@@ -1,47 +1,42 @@
-import React, {useCallback, useState} from 'react'
-import showToast from '@/utils/showToast'
-import {useMagic} from '../MagicProvider'
-import Spinner from '@/components/ui/Spinner'
+import React, { useCallback, useState } from 'react';
+import showToast from '@/utils/showToast';
+import { useMagic } from '../MagicProvider';
+import Spinner from '@/components/ui/Spinner';
 
 const GetMetadata = () => {
-	const {magic} = useMagic()
-	const [disabled, setDisabled] = useState(false)
+  const { magic } = useMagic();
+  const [disabled, setDisabled] = useState(false);
 
-	const getMetadata = useCallback(async () => {
-		if (!magic) return
-		try {
-			setDisabled(true)
-			const userInfo = await magic.user.getMetadata()
-			setDisabled(false)
-			showToast({
-				message: `Public Address: ${userInfo.publicAddress}`,
-				type: 'success',
-			})
-		} catch (error) {
-			setDisabled(false)
-			console.error(error)
-		}
-	}, [magic])
+  const getMetadata = useCallback(async () => {
+    if (!magic) return;
+    try {
+      setDisabled(true);
+      const userInfo = await magic.user.getMetadata();
+      setDisabled(false);
+      showToast({
+        message: `Public Address: ${userInfo.publicAddress}`,
+        type: 'success',
+      });
+    } catch (error) {
+      setDisabled(false);
+      console.error(error);
+    }
+  }, [magic]);
 
-	return (
-		<div className='wallet-method-container'>
-			<button
-				className='wallet-method'
-				onClick={getMetadata}
-				disabled={disabled}>
-				{disabled ? (
-					<div className='loadingContainer' style={{width: '220px'}}>
-						<Spinner />
-					</div>
-				) : (
-					'getMetadata()'
-				)}
-			</button>
-			<div className='wallet-method-desc'>
-				Retrieves information for the authenticated user.
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="wallet-method-container">
+      <button className="wallet-method" onClick={getMetadata} disabled={disabled}>
+        {disabled ? (
+          <div className="loading-container w-[220px]">
+            <Spinner />
+          </div>
+        ) : (
+          'getMetadata()'
+        )}
+      </button>
+      <div className="wallet-method-desc">Retrieves information for the authenticated user.</div>
+    </div>
+  );
+};
 
-export default GetMetadata
+export default GetMetadata;
